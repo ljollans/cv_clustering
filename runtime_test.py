fe8f0bc5315fa98a47a4a198cdd9444dd0a31b20
ljollans <lee.jollans@gmail.com>
2020-03-26 18:53:55 +0100

    the order in which the subjects are in the matrix seems important -- the LOOCV iterations where assignments have highest overlap are close together, i.e. subjects form the same groups were excluded in those iterations.

--- runtime_test.py
+++ runtime_test.py
@@ -18,20 +18,17 @@ with open(pkl_filename, "rb") as file:
     A = pickle.load(file)
 A = np.squeeze(A[:, n_groups - 2, :])
 
-use_subs = np.where(cv_assignment[:, 0] > 0)[0]
-A_use = A[use_subs, :]
-A_use = A_use[:, use_subs]
-print(A_use.shape)
-for i in range(len(use_subs)):
-    where_nan = np.where(np.isnan(A_use[i, :]))[0]
-    row_mean = np.nanmean(A_use[i, :])
-    for nns in where_nan:
-        A_use[i, nns] = row_mean
+assignments_to_use=np.where(cv_assignment[:,0]>0)[0]
+A=A[assignments_to_use,:]
+A=A[:,assignments_to_use]
 
-y_pred = KMeans(n_clusters=n_groups).fit_predict(A_use[:20,:].T)
+#ss=np.linspace(0,len(assignments_to_use)-1,len(assignments_to_use))
+#np.random.shuffle(ss)
+#A=A[ss.astype(int),:]
+
+co_cluster_count, overlap = many_co_cluster_match(A, n_groups)
 
 fig=plt.figure()
-for clus in range(n_groups):
-    plt.subplot(2,3,clus+1)
-    plt.imshow(A_use[:,np.where(y_pred==clus)[0]])
+plt.subplot(1,2,1); plt.imshow(A);
+plt.subplot(1,2,2); plt.imshow(co_cluster_count);
 plt.show()
