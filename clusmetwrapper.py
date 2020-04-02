multi_logr_bag.py problem fixed -- nans in prediction vector from no classification made due to no members of that cluster. added also some initial code for M3C PAC calculation

clusmetwrapper.py
@@ -19,6 +19,7 @@ def clusmetwrapper(design):
     auc = np.full([n_samples, design["n_ks"]], np.nan)
     f1 = np.full([n_samples, design["n_ks"]], np.nan)
     betas = np.full([n_samples, design["n_ks"],  n_features, design["n_ks"] + 2], np.nan)
+    n_per_classification = np.full([n_samples, design["n_ks"], design["n_ks"] + 2], np.nan)
 
     cv_assignment = design["cv_assignment"]
     train_index_sub = np.where(
@@ -36,9 +37,10 @@ def clusmetwrapper(design):
             cal[train_index_sub, nclus],
             auc[train_index_sub, nclus],
             f1[train_index_sub, nclus],
-            betas[train_index_sub, nclus, :, :nclus + 2]
+            betas[train_index_sub, nclus, :, :nclus + 2],
+            n_per_classification[train_index_sub,nclus,:nclus+2]
         ) = loocv_loop(x2, design["covariance"], nclus)
         for t in range(len(train_index_sub)):
             all_clus_labels[train_index_sub[t],nclus,train_index_sub]=tmp_all_clus_labels[t,:]
 
-    return all_clus_labels, bic, sil, cal, auc, f1, betas
+    return all_clus_labels, bic, sil, cal, auc, f1, betas,n_per_classification

looco_loop.py
@@ -14,8 +14,10 @@ def loocv_loop(x_all, covariance, k):
     loocv_auc = np.full([n_samples], np.nan)
     loocv_f1 = np.full([n_samples], np.nan)
     loocv_betas = np.full([n_samples, n_features, k + 2], np.nan)
+    loocv_n_across_cv_folds=np.full([n_samples,k+2],np.nan)
 
-    for loocv in range(n_samples):
+    for loocv in range(5):#n_samples):
+        print('loocv i=',loocv)
         x = np.delete(x_all, loocv, axis=0)
         save_vector = np.delete(np.arange(n_samples),loocv)
         [
@@ -26,6 +28,7 @@ def loocv_loop(x_all, covariance, k):
                 loocv_auc[loocv],
                 loocv_f1[loocv],
                 loocv_betas[loocv, :, :],
+                loocv_n_across_cv_folds[loocv,:]
             ] = calculate_clustering_metrics({'data':x, 'nclus':k, 'covariance':covariance})
 
     return (
@@ -36,4 +39,5 @@ def loocv_loop(x_all, covariance, k):
         loocv_auc,
         loocv_f1,
         loocv_betas,
+        loocv_n_across_cv_folds
     )

multi_logr_bag.py
@@ -33,6 +33,7 @@ def multi_logr_bagr(nboot, xy, n_groups, n_cv_folds, print_output):
     betas_per_fold = np.full([x.shape[1], n_groups, n_cv_folds], np.nan)
     auc_across_cv_folds = np.full([len(set(y))], np.nan)
     f1_across_cv_folds = np.full([len(set(y))], np.nan)
+    n_across_cv_folds = np.full([len(set(y))], np.nan)
 
     fold = -1
     for train_index, test_index in kf.split(x):
@@ -62,12 +63,14 @@ def multi_logr_bagr(nboot, xy, n_groups, n_cv_folds, print_output):
         truth = np.zeros(shape=[len(y)])
         truth[np.where(y == n)[0]] = 1
         groups_in_prediction=len(set(overall_prediction_discrete[:, n]))
+
         if groups_in_prediction > 1:
-            try:
-                auc_across_cv_folds[n] = roc_auc_score(truth, overall_prediction_discrete[:, n])
-                f1_across_cv_folds[n] = f1_score(truth, overall_prediction_discrete[:, n])
-            except:
-                print(overall_prediction_discrete[:, n])
+            prediction = overall_prediction_discrete[:,n]
+            truth=np.delete(truth,np.where(np.isnan(prediction))[0])
+            prediction = np.delete(prediction, np.where(np.isnan(prediction))[0])
+            n_across_cv_folds[n]=len(prediction)
+            auc_across_cv_folds[n] = roc_auc_score(truth, prediction)
+            f1_across_cv_folds[n] = f1_score(truth, prediction)
         else:
             print('WARNING! there were ' + str(len(set(overall_prediction_discrete[:,n]))) + ' groups predicted with truth having ' + str(len(set(truth))) + ' groups.')
         if print_output == 1:
@@ -84,6 +87,7 @@ def multi_logr_bagr(nboot, xy, n_groups, n_cv_folds, print_output):
         auc_per_cv_fold,
         f1_per_cv_fold,
         betas_per_fold,
+        n_across_cv_folds
     )
 
 

pipeline_test.py
@@ -61,9 +61,9 @@ elif do_test==2:
         "mainfold": 0,
         "subfold": 1,
         "covariance": 'full',
-        "n_ks": 2,
+        "n_ks": 5,
     }
-    all_clus_labels, bic, sil, cal, auc, f1, betas = clusmetwrapper(design)
+    all_clus_labels, bic, sil, cal, auc, f1, betas, n_per_classification = clusmetwrapper(design)
     print(np.mean(bic,axis=0))
 
 