edcc2ed6cf007c3b2ea9b443fb103ed13fc0cbb9
ljollans <lee.jollans@gmail.com>
2020-04-07 13:11:05 +0200

    loocv results plotting in class cluster

--- clusmetwrapper.py
+++ clusmetwrapper.py
@@ -2,6 +2,10 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import os
+import pickle
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 
 from looco_loop import loocv_loop
 
@@ -17,8 +21,6 @@ class cluster:
         self.subfold = subfold
         self.covariance = covariance
 
-    def run(self):
-
         self.n_samples = self.data.shape[0]
         self.n_features = self.data.shape[1]
 
@@ -28,17 +30,20 @@ class cluster:
         self.cal = np.full([self.n_samples, self.nk], np.nan)
         self.auc = np.full([self.n_samples, self.nk], np.nan)
         self.f1 = np.full([self.n_samples, self.nk], np.nan)
-        self.betas = np.full([self.n_samples, self.nk,  self.n_features, self.nk + 2], np.nan)
+        self.betas = np.full([self.n_samples, self.nk, self.n_features, self.nk + 2], np.nan)
         self.n_per_classification = np.full([self.n_samples, self.nk, self.nk + 2], np.nan)
 
         self.train_index_sub = np.where(
             (self.cv_assignment[:, self.mainfold] != self.subfold)
             & (~np.isnan(self.cv_assignment[:, self.mainfold]))
         )[0]
+
+    def run(self):
+
         x2 = self.data[self.train_index_sub, :]
 
         for nclus in range(self.nk):
-            print(nclus+2,'clusters')
+            print(nclus + 2, 'clusters')
             (
                 tmp_all_clus_labels,
                 self.bic[self.train_index_sub, nclus],
@@ -47,8 +52,53 @@ class cluster:
                 self.auc[self.train_index_sub, nclus],
                 self.f1[self.train_index_sub, nclus],
                 self.betas[self.train_index_sub, nclus, :, :nclus + 2],
-                self.n_per_classification[self.train_index_sub,nclus,:nclus+2]
+                self.n_per_classification[self.train_index_sub, nclus, :nclus + 2]
             ) = loocv_loop(x2, self.covariance, nclus)
             for t in range(len(self.train_index_sub)):
-                self.all_clus_labels[self.train_index_sub[t],nclus,self.train_index_sub]=tmp_all_clus_labels[t,:]
+                self.all_clus_labels[self.train_index_sub[t], nclus, self.train_index_sub] = tmp_all_clus_labels[t, :]
+
+    def pull_in_saves(self, bic, sil, cal, all_clus_labels, auc, f1, betas):
+
+        self.bic = bic
+        self.sil = sil
+        self.cal = cal
+        self.all_clus_labels = all_clus_labels
+        self.auc = auc
+        self.f1 = f1
+        self.betas = betas
+
+    def plot_loocv(self):
+        df = pd.DataFrame({}, columns=['bic','sil', 'auc', 'f1', 'k', 'ppt'])
+        for nclus in range(self.nk):
+            tmp_df = pd.DataFrame(
+                {'bic': np.squeeze(self.bic[:, nclus]),
+                 'sil': np.squeeze(self.sil[:, nclus]),
+                 'auc': np.squeeze(self.auc[:, nclus]),
+                 'f1': np.squeeze(self.f1[:, nclus]),
+                 'k': np.ones(shape=[398]) * nclus + 2,
+                 'ppt': np.arange(398)},
+                columns=['bic','sil', 'auc', 'f1', 'k', 'ppt'])
+            df = df.append(tmp_df)
+
+        sns.set()
+        fig = plt.figure()
+        plot_these = ['bic', 'sil', 'auc', 'f1']
+        call_these = ['BIC', 'Silhouette score', 'AUC', 'F1 score']
+        for p in range(len(plot_these)):
+            plt.subplot(2, 2, p + 1);
+            sns.violinplot('k', plot_these[p], data=df)
+            plt.title(call_these[p]);
+            plt.xticks(np.arange(self.nk), np.arange(self.nk) + 2)
+            plt.xlabel("k")
+        plt.show()
+
+    def aggregate_loocv(self):
+        A=self.all_clus_labels
+        print(A.shape)
+
+
+
+
+
+
 
--- runtime_test.py
+++ runtime_test.py
@@ -1,94 +1,72 @@
-from loocv_assigmatcher_nov import (
-    get_k_from_bic,
-    getloopcount,
-    plot_bic_violin,
-    get_clusassignments_from_LOOCV,
-    get_co_cluster_count,
-    get_final_assignment,
-    get_maxmatches,
-    k_workup_mainfold,
-    match_assignments_to_final_assignments,
-    get_aggregated_patterns,
-    recode_iteration_assignments,
-    rand_score_comparison,
-    collect_betas_for_corresponding_clus,
-    n_clus_retrieval_chk)
-import pickle
 import csv
-import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-import seaborn as sns
-from sklearn.metrics import adjusted_rand_score as rand
+import sys
+import time
+from clusmetwrapper import cluster
+from utils import identify_set_and_fold
+import pickle
+import matplotlib.pyplot as plt
 
-from utils import ecdf, sdremoved_highest_val, max_min_val_check, rand_score_withnans
+seconds1 = time.time()
 
-set = 5
-subfold = 1
+input_files_dir = "/Users/lee_jollans/Projects/clustering_pilot/residfiles_all_210220/"
+cv_assignment_dir = "/Users/lee_jollans/Documents/GitHub/ML_in_python/export_251019/"
+
+# presets
+sets = [
+    "Tc",
+    "Sc",
+    "TSc",
+    "Tc_tc",
+    "Sc_sc",
+    "TSc_tsc",
+    "Tct_s",
+    "Scs_s",
+    "Tct_Scs_s",
+    "Tct_tc_s",
+    "Scs_sc_s",
+    "Tct_Scs_tc_sc_s",
+]
+
+n_cv_folds = 4
+n_ks = 8
+current_set=0
+
+# cv import
+with open((cv_assignment_dir + "CVassig398.csv"), "r") as f:
+    reader = csv.reader(f, delimiter=",")
+    cv_assignment = np.array(list(reader)).astype(float)
+
+# data import
+if current_set >= len(sets):
+    data_path = input_files_dir + "MDD__" + sets[current_set - len(sets)] + "_ctrl.csv"
+else:
+    data_path = input_files_dir + "MDD__" + sets[current_set] + ".csv"
+with open(data_path, "r") as f:
+    reader = csv.reader(f, delimiter=",")
+    data = np.array(list(reader)).astype(float)
+
+# init mod
 mainfold = 0
+subfold = 0
+mod = cluster(data, n_ks, cv_assignment, mainfold, subfold, "full")
 
-k = 6
+#pull in results
+savedir = ('/Users/lee_jollans/Projects/clustering_pilot/FEB_PUT/FEB_' + sets[current_set])
+ctr = 0
+fold = (mainfold * 4) + subfold
+saveasthese = ["bic","sil","cal","all_clus_labels","auc","f1","betas"]
+loadasthese = ["BIC","SIL","CAL","allcluslabels","AUC","F1","BETAS"]
+for d in range(len(saveasthese)):
+    if ctr == 1:
+        fold_string = "ctrl_fold" + str(fold) + ".pkl"
+    else:
+        fold_string = "_fold" + str(fold) + ".pkl"
+    pkl_filename = savedir + loadasthese[d] + fold_string
+    with open(pkl_filename, "rb") as file:
+        tmp = pickle.load(file)
+    exec(saveasthese[d] + " = tmp")
 
-sets = ['Tvc', 'Svc', 'TSvc', 'Tvc_tvc', 'Svc_svc', 'TSvc_tsvc', 'Tvct_s', 'Svcs_s', 'Tvct_Svcs_s', 'Tvct_tvc_s',
-            'Svcs_svc_s', 'Tvct_Svcs_tvc_svc_s']
-sets_null = ['Tvc', 'Svc', 'TSvc', 'Tvc_tvc', 'Svc_svc', 'TSvc_tsvc', 'Tvct', 'Svcs', 'Tvct_Svcs', 'Tvct_tvc',
-            'Svcs_svc', 'Tvct_Svcs_tvc_svc']
-savedir_null='/Users/lee_jollans/Projects/clustering_pilot/null/JAN1_'
-savedir='/Users/lee_jollans/Projects/clustering_pilot//FEB_PUT/FEB_'
-
-A2 = get_clusassignments_from_LOOCV(set, mainfold, subfold, sets, savedir, 0)
-A = A2
-
-A2 = get_clusassignments_from_LOOCV(set, mainfold, subfold, sets_null, savedir_null, 1)
-A_null = A2
-
-ex1=getloopcount(savedir, 0)
-ex1_null=getloopcount(savedir_null, 1)
-[bestSIL, bestCAL, bestBIC,bestnclus_mf, bestnclus_sf, best_k_loocv, SIL, CAL, BIC, pct_agreement_k] = get_k_from_bic(savedir, ex1, 0, sets)
-[bestSIL_null, bestCAL_null,bestBIC_null, bestnclus_mf_null, bestnclus_sf_null,best_k_loocv_null, SIL_null, CAL_null, BIC_null, pct_agreement_k_null] = \
-    get_k_from_bic(savedir_null, ex1_null, 1, sets_null)
-
-cr, pac, plateau_cr = n_clus_retrieval_chk(A, .7, 25)
-cr_null, pac_null, plateau_cr_null = n_clus_retrieval_chk(A_null, .7, 25)
-
-fig = plt.figure();
-plt.subplot(2,2,1); plt.title('actual bic');  plot_bic_violin(BIC, mainfold, subfold, 0, set);
-plt.subplot(2,2,2); plt.title('null bic');  plot_bic_violin(BIC_null, mainfold, subfold, 0, set);
-plt.subplot(2,2,3); plt.title('saturation cluster')
-plt.plot(cr); plt.plot(cr_null); plt.legend(['actual','null'])
-plt.subplot(2,2,4); plt.title('pac')
-plt.plot(pac_null-pac); plt.legend(['null-actual'])
-plt.show()
-
-
-#match_pct = match_assignments_to_final_assignments(A, final_assignment)  # fit of all observations to these patterns
-#meet_fit_requirements = max_min_val_check(match_pct, 50, 25)  # observations that can clearly be assigned to one of the patterns
-#print(len(meet_fit_requirements),"observations meet fit requirements for overlap with final assignments",)
-#aggregated_patterns_arrays = get_aggregated_patterns(final_assignment, meet_fit_requirements, match_pct, A)#
-
-#corresponding_cluster = recode_iteration_assignments(aggregated_patterns_arrays, k)
-
-
-#n_groups = corresponding_cluster.shape[1]
-#n_iterations = A.shape[0]
-#n_obs = A.shape[1]
-#checkclus = np.zeros(shape=[n_groups, n_iterations, n_obs])
-#consensus_label = np.full(n_obs, np.nan)
-#for group in range(n_groups):
-#    for i in range(n_iterations):
-#        checkclus[group, i, np.where(A[i, :] == corresponding_cluster[i, group])[0]] = 1
-#for ppt in range(n_obs):
-#    crit = np.sum(checkclus[:, :, ppt], axis=1)
-#    consensus_label[ppt] = np.where(crit == np.max(crit))[0][0]
-
-
-#aggregated_betas, new_betas = collect_betas_for_corresponding_clus(
-#    corresponding_cluster, set, mainfold, subfold, k
-#)
-
-#fig = plt.figure()
-#plt.plot(aggregated_betas)
-#plt.show()
-
-
-# rand_all, rand_aftermatch = rand_score_comparison(A,consensus_label)
+mod.pull_in_saves(bic, sil, cal, all_clus_labels, auc, f1, betas)
+#mod.plot_loocv()
+mod.aggregate_loocv()
